68826993fffca712c5cc05c77e146138e34d6e9a
inkfarer <inkfarer@inkfarer.com>
2022-06-10 23:04:29 +0300

    Revert "Only allow two casters to be created"
    This reverts commit 83f0e8120e17277061da22067acbeee46b04bf22.

--- src/browser-common/store/CasterStore.ts
+++ src/browser-common/store/CasterStore.ts
@@ -1,5 +1,7 @@
 import { Caster, Casters } from 'schemas';
+import { generateId } from '@helpers/generateId';
 import { defineStore } from 'pinia';
+import omit from 'lodash/omit';
 
 export const castersReps = [
     nodecg.Replicant<Casters>('casters')
@@ -7,15 +9,42 @@ export const castersReps = [
 
 export interface CasterStore {
     casters: Casters;
+    uncommittedCasters: Caster[];
 }
+
+const defaultCaster = {
+    name: 'New Caster',
+    twitter: '',
+    pronouns: '?/?'
+};
 
 export const useCasterStore = defineStore('casters', {
     state: () => ({
-        casters: []
+        casters: [],
+        uncommittedCasters: []
     } as CasterStore),
     actions: {
         updateCaster(caster: Caster): void {
             nodecg.sendMessage('casters:save', caster);
+        },
+        removeUncommittedCaster(id: string): void {
+            this.uncommittedCasters = (this.uncommittedCasters as Caster[]).filter(caster => caster.id !== id);
+        },
+        addUncommittedCaster({ id, caster }: { id: string, caster?: Caster }): void {
+            this.uncommittedCasters.push({ id, ...(caster ?? defaultCaster) });
+        },
+        addDefaultCaster(): string {
+            const id = generateId();
+            this.addUncommittedCaster({ id });
+            return id;
+        },
+        async saveUncommittedCaster(id: string): Promise<string> {
+            const newCaster = await nodecg.sendMessage('casters:save', omit((this.uncommittedCasters as Caster[]).find(caster => caster.id === id), ['id']));
+            this.removeUncommittedCaster(id);
+            return newCaster.id;
+        },
+        async removeCaster(id: string): Promise<void> {
+            return nodecg.sendMessage('casters:delete', id);
         }
     }
 });
