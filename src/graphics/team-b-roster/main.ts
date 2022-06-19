365c753da6f2f363799d0bda1aaea78351a021f6
inkfarer <inkfarer@inkfarer.com>
2022-06-19 16:18:06 +0300

    Add single team roster graphics

---
+++ src/graphics/team-b-roster/main.ts
@@ -0,0 +1,21 @@
+import '../styles/graphicStyles.scss';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { setUpReplicants } from '@browser-common/store/storeHelper';
+import { activeMatchStoreReps, useActiveMatchStore } from '@browser-common/store/ActiveMatchStore';
+import { castersReps, useCasterStore } from '@browser-common/store/CasterStore';
+import { assignAccentColor, runtimeConfigReps, useRuntimeConfigStore } from '@browser-common/store/RuntimeConfigStore';
+import SingleRosterGraphic from '../singleroster/SingleRosterGraphic.vue';
+
+(async () => {
+    const app = createApp(SingleRosterGraphic, {
+        team: 'B'
+    });
+    app.use(createPinia());
+    await setUpReplicants(activeMatchStoreReps, useActiveMatchStore());
+    await setUpReplicants(castersReps, useCasterStore());
+    await setUpReplicants(runtimeConfigReps, useRuntimeConfigStore());
+    assignAccentColor();
+    app.mount('#app');
+})();
