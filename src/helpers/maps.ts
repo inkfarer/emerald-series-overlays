d986d426081dbdd478360ddf502de036eea0c465
inkfarer <inkfarer@inkfarer.com>
2022-07-10 14:44:35 +0300

    Make the game mode of map into an enum value

--- src/helpers/maps.ts
+++ src/helpers/maps.ts
@@ -1,9 +1,11 @@
+import { GameMode } from '../types/enums/GameMode';
+
 export interface BridgeMap {
     name: string
-    mode?: string
+    mode?: GameMode
 }
 
-export const buckyMaps = [
+export const buckyMaps: BridgeMap[] = [
     { name: 'Urban' },
     { name: 'Boo' },
     { name: 'Treehouse' },
@@ -17,34 +19,34 @@ export const buckyMaps = [
     { name: 'Unknown' }
 ];
 
-export const stratusMaps = [
-    { mode: 'KotH', name: 'Smoke KotH' },
-    { mode: 'KotH', name: 'Coven' },
-    { mode: 'KotH', name: 'Topaz' },
-    { mode: 'KotH', name: 'Ultraviolet' },
-    { mode: 'KotH', name: 'Willow' },
-    { mode: 'KotH', name: 'Concorde' },
-    { mode: 'KotH', name: 'Industrial' },
-    { mode: 'CTF', name: 'Rusteze' },
-    { mode: 'CTF', name: 'Hammerhead' },
-    { mode: 'CTF', name: 'Ruins' },
-    { mode: 'CTF', name: 'Mechanized' },
-    { mode: '5CP', name: 'LeBronze' },
-    { mode: '5CP', name: 'Marzipan' },
-    { mode: '5CP', name: 'Cherokee' },
-    { mode: 'KotF', name: 'Revolution' },
-    { mode: 'KotF', name: 'Desert Sanctuary' },
-    { mode: 'KotF', name: 'Splinter' },
-    { mode: 'KotF', name: 'Shinobi' },
-    { mode: 'KotF', name: 'West Rider' },
-    { mode: 'CTP', name: 'Moonshine' },
+export const stratusMaps: BridgeMap[] = [
+    { mode: GameMode.KOTH, name: 'Smoke KotH' },
+    { mode: GameMode.KOTH, name: 'Coven' },
+    { mode: GameMode.KOTH, name: 'Topaz' },
+    { mode: GameMode.KOTH, name: 'Ultraviolet' },
+    { mode: GameMode.KOTH, name: 'Willow' },
+    { mode: GameMode.KOTH, name: 'Concorde' },
+    { mode: GameMode.KOTH, name: 'Industrial' },
+    { mode: GameMode.CTF, name: 'Rusteze' },
+    { mode: GameMode.CTF, name: 'Hammerhead' },
+    { mode: GameMode.CTF, name: 'Ruins' },
+    { mode: GameMode.CTF, name: 'Mechanized' },
+    { mode: GameMode.FIVE_CP, name: 'LeBronze' },
+    { mode: GameMode.FIVE_CP, name: 'Marzipan' },
+    { mode: GameMode.FIVE_CP, name: 'Cherokee' },
+    { mode: GameMode.KOTF, name: 'Revolution' },
+    { mode: GameMode.KOTF, name: 'Desert Sanctuary' },
+    { mode: GameMode.KOTF, name: 'Splinter' },
+    { mode: GameMode.KOTF, name: 'Shinobi' },
+    { mode: GameMode.KOTF, name: 'West Rider' },
+    { mode: GameMode.CTP, name: 'Moonshine' },
     { name: 'Unknown' }
 ];
 
-export const modeNames = {
-    KotH: 'King of the Hill',
-    CTF: 'Capture the Flag',
-    '5CP': 'Five Control Points',
-    KotF: 'King of the Flag',
-    CTP: 'Capture the Point'
+export const modeNames: { [key in GameMode]: string } = {
+    [GameMode.KOTH]: 'King of the Hill',
+    [GameMode.CTF]: 'Capture the Flag',
+    [GameMode.FIVE_CP]: 'Five Control Points',
+    [GameMode.KOTF]: 'King of the Flag',
+    [GameMode.CTP]: 'Capture the Point'
 };
---
+++ src/types/enums/GameMode.ts
@@ -0,0 +1,7 @@
+export enum GameMode {
+    KOTH = 'KotH',
+    CTF = 'CTF',
+    FIVE_CP = '5CP',
+    KOTF = 'KotF',
+    CTP = 'CTP'
+}
