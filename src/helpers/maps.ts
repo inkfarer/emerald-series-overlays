410059163cbfbcd5fedfb8db463a2223482a283b
inkfarer <inkfarer@inkfarer.com>
2022-07-16 17:33:07 +0300

    Bump version and remove unneeded maps

--- src/helpers/maps.ts
+++ src/helpers/maps.ts
@@ -21,24 +21,11 @@ export const buckyMaps: BridgeMap[] = [
 
 export const stratusMaps: BridgeMap[] = [
     { mode: GameMode.KOTH, name: 'Smoke KotH' },
-    { mode: GameMode.KOTH, name: 'Coven' },
     { mode: GameMode.KOTH, name: 'Topaz' },
-    { mode: GameMode.KOTH, name: 'Ultraviolet' },
-    { mode: GameMode.KOTH, name: 'Willow' },
-    { mode: GameMode.KOTH, name: 'Concorde' },
-    { mode: GameMode.KOTH, name: 'Industrial' },
     { mode: GameMode.CTF, name: 'Rusteze' },
-    { mode: GameMode.CTF, name: 'Hammerhead' },
-    { mode: GameMode.CTF, name: 'Ruins' },
-    { mode: GameMode.CTF, name: 'Mechanized' },
-    { mode: GameMode.FIVE_CP, name: 'LeBronze' },
     { mode: GameMode.FIVE_CP, name: 'Marzipan' },
-    { mode: GameMode.FIVE_CP, name: 'Cherokee' },
     { mode: GameMode.KOTF, name: 'Revolution' },
-    { mode: GameMode.KOTF, name: 'Desert Sanctuary' },
-    { mode: GameMode.KOTF, name: 'Splinter' },
     { mode: GameMode.KOTF, name: 'Shinobi' },
-    { mode: GameMode.KOTF, name: 'West Rider' },
     { mode: GameMode.CTP, name: 'Moonshine' },
     { name: 'Unknown' }
 ];
